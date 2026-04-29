dc756202bf55d059fa13156cfd16bbf4bc371bca
ilmaribc <s2300904@edu.bc.fi>
2026-04-29 13:42:06 +0300

    Terrain shit

---
+++ Bloodmoon Alpha 0.01/Assets/Scripts/Terrain/Slow TextureGeneration Manager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SlowTextureGenerationManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Terrains.TextureGenerators.Count == 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Terrains.TextureGenerators[Terrains.TextureGenerators.Count - 1].MaterialChange();
+            Terrains.TextureGenerators.RemoveAt(Terrains.TextureGenerators.Count - 1);
+            Debug.Log("Tiles Left: " + Terrains.TextureGenerators.Count);
+        }
+    }
+}
