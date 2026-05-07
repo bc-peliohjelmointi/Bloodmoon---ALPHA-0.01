fa5186a89a5d02dc9a9633b7aa06b98fa0c53f12
ilmaribc <s2300904@edu.bc.fi>
2026-05-07 10:48:43 +0300

    Player can collect water from lake

---
+++ Bloodmoon Alpha 0.01/Assets/Scripts/Player/CollectWater.cs
@@ -0,0 +1,56 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CollectWater : MonoBehaviour
+{
+    GameObject WaterCollectionMarker;
+    PlayerInput Input;
+    Inventory Inv;
+    Item Water;
+    bool WaterCollected = false;
+    private void Start()
+    {
+        foreach (var item in GetComponent<SaveMyStuff>().items) 
+        {
+            if (item.name == "Water") 
+            {
+                Water = item;
+                break;
+            }
+        }
+        Inv = GetComponent<Inventory>();
+        Input = GameObject.Find("Character").GetComponent<PlayerInput>();
+        WaterCollectionMarker = GameObject.Find("Water Colection marker");
+    }
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 5f))
+        {
+            if (hit.transform.tag == "Water")
+            {
+                WaterCollectionMarker.active = true;
+                if (Input.actions.FindAction("Interact").IsPressed())
+                {
+                    if (!WaterCollected) 
+                    {
+                        Inv.SpawnInventoryItem(Water);
+                        WaterCollected=true;
+                    }
+                }
+                else
+                {
+                    WaterCollected = false;
+                }
+            }
+            else
+            {
+                WaterCollectionMarker.active = false;
+            }
+        }
+        else
+        {
+            WaterCollectionMarker.active = false;
+        }
+    }
+}
